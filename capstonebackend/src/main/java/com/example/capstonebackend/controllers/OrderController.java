1b948eefe7a4fd702715acfdc273a42bc18d7c77
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-11 15:23:51 +0100

    added orderService and packageService

--- capstonebackend/src/main/java/com/example/capstonebackend/controllers/OrderController.java
+++ capstonebackend/src/main/java/com/example/capstonebackend/controllers/OrderController.java
@@ -1,17 +1,17 @@
 package com.example.capstonebackend.controllers;
 
+import com.example.capstonebackend.models.Order;
 import com.example.capstonebackend.services.OrderService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("orders")
 public class OrderController {
-    @Autowired
-    OrderService orderService;
-
-
-
-
 }
--- capstonebackend/src/main/java/com/example/capstonebackend/services/OrderService.java
+++ capstonebackend/src/main/java/com/example/capstonebackend/services/OrderService.java
@@ -1,7 +1,40 @@
 package com.example.capstonebackend.services;
 
+import com.example.capstonebackend.models.Order;
+import com.example.capstonebackend.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(Long id){
+        return orderRepository.findById(id);
+    }
+
+    public Order createOrder(Order order){
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(Long id, Order order){
+        if (orderRepository.existsById(id)){
+            order.setId(id);
+            return orderRepository.save(order);
+        }
+        return null;
+    }
+
+    public void deleteOrder(Long id){
+        orderRepository.deleteById(id);
+    }
 }
