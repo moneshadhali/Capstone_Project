3a92290c3837f45e84fb319f2148570267a471a1
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-04-11 11:02:58 +0100

    created models for order and package

--- capstonebackend/src/main/java/com/example/capstonebackend/models/Order.java
+++ capstonebackend/src/main/java/com/example/capstonebackend/models/Order.java
@@ -1,4 +1,101 @@
 package com.example.capstonebackend.models;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column
+    private String postcode;
+    @Column
+    private String address;
+    @Column
+    private Boolean isAccepted;
+    @Column
+    private Boolean isDelivered;
+    @Column
+    private String  customerName;
+    @Column
+    private float volume;
+
+    @OneToMany(mappedBy = "order")
+    private List <Package> packageList;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    public Order(Long id, String postcode, String address, Boolean isAccepted, Boolean isDelivered, String customerName, float volume) {
+        this.id = id;
+        this.postcode = postcode;
+        this.address = address;
+        this.isAccepted = isAccepted;
+        this.isDelivered = isDelivered;
+        this.customerName = customerName;
+        this.volume = volume;
+    }
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Boolean getAccepted() {
+        return isAccepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        isAccepted = accepted;
+    }
+
+    public Boolean getDelivered() {
+        return isDelivered;
+    }
+
+    public void setDelivered(Boolean delivered) {
+        isDelivered = delivered;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
 }
