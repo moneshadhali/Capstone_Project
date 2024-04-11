cb989a890303ef345efe5535bebdf58d57aeedc6
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-11 15:24:22 +0100

    added partial orderController

--- capstonebackend/src/main/java/com/example/capstonebackend/controllers/OrderController.java
+++ capstonebackend/src/main/java/com/example/capstonebackend/controllers/OrderController.java
@@ -14,4 +14,42 @@ import java.util.Optional;
 @RestController
 @RequestMapping("orders")
 public class OrderController {
+    @Autowired
+    OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orders = orderService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id){
+        Optional<Order> foundOrder = orderService.getOrderById(id);
+        if (foundOrder.isPresent()){
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+//    @PostMapping("/{id}")
+//    public ResponseEntity<Order> updateOrder(@RequestBody Order order, @PathVariable Long id){
+//        Order newOrder = orderService.updateOrder(order, id);
+//        return new ResponseEntity<>(order, HttpStatus.CREATED);
+//    }
+//
+//    @PutMapping (value = "/{id}")
+//    public ResponseEntity<Order> updateChocolate(@RequestBody Order order, @PathVariable Long id){
+//        Order order = orderService.updateOrder(order, id);
+//        return new ResponseEntity<>(order, HttpStatus.OK);
+//    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteOrder(@PathVariable Long id){
+        orderService.deleteOrder(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+
+
 }
