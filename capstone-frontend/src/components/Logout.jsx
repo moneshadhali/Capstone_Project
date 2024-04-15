604fca7e2bee34c2b9f03b07519fca101772f900
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-04-15 13:41:23 +0100

    added logout button

---
+++ capstone-frontend/src/components/Logout.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+const Logout =  () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        navigate("http://localhost:8080")
+
+    }
+
+    return ( 
+        <button onClick={handleLogout}> Logout </button>
+     );
+}
+ 
+export default Logout;
