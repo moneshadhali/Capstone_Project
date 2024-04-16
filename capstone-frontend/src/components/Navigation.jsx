c793e7acae0a79d4481ef76fc5771ec3014d2328
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-16 14:15:37 +0100

    added burgermenu

--- capstone-frontend/src/components/Navigation.jsx
+++ capstone-frontend/src/components/Navigation.jsx
@@ -1,15 +1,31 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
-import Logout from "./Logout";
+import "../styles/navigation.css";
+import { useState } from "react";
 
 const Navigation = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  
   const navigate = useNavigate();
+
   const handleLogout = () => {
     navigate("/");
+    setMenuOpen(false);
   };
+
+  const toggleMenu=() => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="navbar">
+      <button className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <span className="burger"></span>
+                <span className="burger"></span>
+                <span className="burger"></span>
+      </button>
+      <ul className={`menu ${menuOpen ? "open" : ""}`}>
           <li>
             <Link to="/profile">Profile</Link>
           </li>
