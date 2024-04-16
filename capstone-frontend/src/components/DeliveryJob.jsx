3758cdd451839624cbccb3a3cb60cc3b6147f609
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-04-16 10:53:26 +0100

    added packages to order in frontend

--- capstone-frontend/src/components/DeliveryJob.jsx
+++ capstone-frontend/src/components/DeliveryJob.jsx
@@ -8,26 +8,37 @@ const DeliveryJob = ({ job, updateBtn, btnMessage }) => {
     navigate("/my-deliveries");
   };
 
-  return (
-    <section className="job">
-      <div className="job-details">
-        <p>Customer: {job.customerName}</p>
-        <p>Address: {job.address}</p>
-        <p>Postcode: {job.postcode}</p>
-        <p>Order Volume:{job.volume}</p>
-      </div>
-      <div className="map">
-      <iframe
-        title="map"
-        src={`https://maps.google.com/maps?q=${encodeURIComponent(job.address)}&z=15&output=embed`}
-      ></iframe>
-      
-      </div>
-      <div>
-        <button onClick={handleUpdateButton}>{btnMessage}</button>
-      </div>
-    </section>
-  );
+  const packageList = job.packages.map((pack) => {
+    return (
+          <li>{pack.product_name}</li>
+    )
+  })
+
+  if (job) {
+    return (
+
+      <section className="job">
+        <div className="job-details">
+          <p>Customer: {job.customerName}</p>
+          <p>Address: {job.address}</p>
+          <p>Postcode: {job.postcode}</p>
+          <p>Order Volume:{job.volume}</p>
+        </div>
+        <ul>Packages: {packageList}</ul>
+        <div className="map">
+          <iframe
+            title="map"
+            src={`https://maps.google.com/maps?q=${encodeURIComponent(job.address)}&z=15&output=embed`}
+          ></iframe>
+        </div>
+        <div>
+          <button onClick={handleUpdateButton}>{btnMessage}</button>
+        </div>
+      </section>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default DeliveryJob;
