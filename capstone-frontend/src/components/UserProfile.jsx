94524dbc1499a219ce86ea81a4be1b1285ab8678
Monesha <moneshadhali@gmail.com>
2024-04-16 17:08:57 +0100

    added css to profile

--- capstone-frontend/src/components/UserProfile.jsx
+++ capstone-frontend/src/components/UserProfile.jsx
@@ -3,37 +3,32 @@ import "../styles/user-profile.css";
 
 const UserProfile = ({ userProfile }) => {
   if (userProfile) {
-    return (
+    return ( 
       <>
-      <section className="profile">
-        <div className="profile-details">
-        <h1>Profile</h1>
+        <section className="profile">
+          <div className="profile-details">
+            <h1>Profile</h1>
+            <img src="images/profile1.jpg" alt="profile picture" ></img>
+            <div className="name">
+              <p>
+                <span>Name:</span> {userProfile.name}</p>
+            </div>
+            <div className="region">
+              <p>
+                <span>Region:</span> {userProfile.region}</p>
+            </div>
+            <div className="location">
+              <p>
+                <span>Warehouse Location: </span> {userProfile.warehouse.location},{" "}
+                {userProfile.warehouse.name}
+              </p>
+            </div>
 
-        <div className="name">
-        <p>
-          <span>Name:</span> {userProfile.name}</p>
-        </div>
+              <button className="edit-btn">
+                <Link to={`/profile/${userProfile.id}/edit`}>Edit</Link>
+              </button>
 
-        <div className="region">
-        <p>
-          <span>Region:</span> {userProfile.region}</p>
-        </div>
-
-        <div className="location">
-        <p>
-
-          <span>Warehouse Location: </span> {userProfile.warehouse.location},{" "}
-          {userProfile.warehouse.name}
-        </p>
-        </div>
-
-        <div className="button">
-        <button className="edit-btn"> 
-          <Link to={`/profile/${userProfile.id}/edit`}>Edit</Link>
-        </button>
-        </div>
-        
-        </div>
+          </div>
         </section>
       </>
     );
