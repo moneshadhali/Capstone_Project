e0fdfe98c82e37d374dd0db93bd826ffb349adec
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-04-17 15:48:58 +0100

    removed redirect upon job acception

--- capstone-frontend/src/components/DeliveryJob.jsx
+++ capstone-frontend/src/components/DeliveryJob.jsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "../styles/delivery-jobs.css";
 import PackageListPopup from "./PackageListPopUp";
@@ -6,38 +5,38 @@ import PackageListPopup from "./PackageListPopUp";
 const DeliveryJob = ({ job, updateBtn, btnMessage }) => {
   const [showPackages, setShowPackages] = useState(false);
 
-  const navigate = useNavigate();
-
   const handleUpdateButton = () => {
     updateBtn(job.id);
-    navigate("/my-deliveries");
   };
 
   if (job) {
     return (
       <section className="job-listing">
-          <p>
-            <span>Recipient: </span> {job.customerName}
-          </p>
-          <p>Address: {job.address}</p>
-          <p>Postcode: {job.postcode}</p>
+        <p>
+          <span>Recipient: </span> {job.customerName}
+        </p>
+        <p>Address: {job.address}</p>
+        <p>Postcode: {job.postcode}</p>
 
-          <button className="view-packages-btn" onClick={() => setShowPackages(true)}>View Packages</button>
-          {showPackages && (
-            <PackageListPopup
-              job={job}
-              onClose={() => setShowPackages(false)}
-            />
-          )}
+        <button
+          className="view-packages-btn"
+          onClick={() => setShowPackages(true)}
+        >
+          View Packages
+        </button>
+        {showPackages && (
+          <PackageListPopup job={job} onClose={() => setShowPackages(false)} />
+        )}
 
-          <button className="accept-btn" onClick={handleUpdateButton}>{btnMessage}</button>
-          <iframe
-            title="map"
-            src={`https://maps.google.com/maps?q=${encodeURIComponent(
-              job.address
-            )}&z=15&output=embed`}
-          ></iframe>
-       
+        <button className="accept-btn" onClick={handleUpdateButton}>
+          {btnMessage}
+        </button>
+        <iframe
+          title="map"
+          src={`https://maps.google.com/maps?q=${encodeURIComponent(
+            job.address
+          )}&z=15&output=embed`}
+        ></iframe>
       </section>
     );
   } else {
